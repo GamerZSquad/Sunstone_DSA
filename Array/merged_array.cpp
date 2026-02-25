1a86e1f67a2bce9c1e16286968c8ab8fa59bd5b9
Sumit kumar <153995480+GamerZSquad@users.noreply.github.com>
2026-02-25 16:09:03 +0530

    h

---
+++ Array/merged_array.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int startStation(vector<int>gas,vector<int> cost){
+    int total=0;
+    int bachihuogas=0;
+    int ans=0;
+
+    for(int i=0;i<gas.size();i++){
+        total+=(gas[i]-cost[i]);
+    }
+}
+
+int main() {
+
+    
+
+    return 0;
+}
